1a20d639d9e56782cef6426c46c03cf753dd9aea
B00M808 <79868861+B00M808@users.noreply.github.com>
2022-09-06 13:54:41 -0700

    Cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 /*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
@@ -32,28 +47,24 @@ function showPage(list, page) {
       }
 
    }
-}
-    // loop over the length of the `list` parameter
-   //for(list.length) {
-      
-   
+}  
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
 function addPagination(list) {
-  // create a variable to calculate the number of   pages needed
+  // created a variable to calculate the number of pages needed
   let numOfPages = list.length / 9;
-  // select the element with a class of `link-list`   and assign it to a variable
+  // selected the element with a class of `link-list` and assigned it to a variable
   let linkList = document.querySelector(".link-list");
 
   // set the innerHTML property of the variable you just created to an empty string
   linkList.innerHTML = "";
 
-  // loop over the number of pages needed
+  // looped over the number of pages needed
   for(let i = 0; i < numOfPages; i++){
-    // create the elements needed to display the pagination button
+    // created the elements needed to display the pagination button
     linkList.insertAdjacentHTML('beforeend', `
     <li>
       <button type="button">${[i + 1]}</button>
@@ -63,7 +74,7 @@ function addPagination(list) {
   }
   linkList.firstElementChild.firstElementChild.className = 'active';
 
-  // Now create an event listener on linkList that will be called when there is a click event.
+  // created an event listener on linkList that will be called when there is a click event
   linkList.addEventListener("click", (e) => {
     if(e.target.localName == "button"){
       document.querySelector('.active').className = "";
@@ -74,11 +85,8 @@ function addPagination(list) {
   })
 
 }
-
-
-  // give the first pagination button a class of "active"
  
    // Call functions
 
-   showPage(data, 1); 
+  showPage(data, 1); 
 addPagination(data)
